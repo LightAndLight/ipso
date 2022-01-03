0ee384863d26781e7e62ca3571e810497a30b1e2
Isaac Elliott <isaace71295@gmail.com>
2022-01-03 18:52:48 +1000

    feat(typecheck): implement unification in kind_inference module

--- ipso-typecheck/src/kind_inference.rs
+++ ipso-typecheck/src/kind_inference.rs
@@ -6,6 +6,7 @@ use ipso_syntax::kind::{Kind, KindCompound};
 pub type Meta = usize;
 
 /// The type of kind metavariable solutions.
+#[derive(Clone)]
 pub enum Solution {
     Unsolved,
     Solved(Kind),
@@ -36,10 +37,14 @@ impl Solutions {
     /**
     Get a metavariable's solution.
 
-    Returns [`None`] if the metavariable is not in the [`Solutions`]' domain.
+    # Preconditions
+
+    * `self.contains(meta)`
     */
-    pub fn get(&self, meta: Meta) -> Option<&Solution> {
-        self.solutions.get(meta)
+    pub fn get(&self, meta: Meta) -> &Solution {
+        self.solutions
+            .get(meta)
+            .unwrap_or_else(|| panic!("meta {:?} not found", meta))
     }
 
     /**
@@ -50,20 +55,17 @@ impl Solutions {
     # Preconditions
 
     * `self.contains(meta)`
-    * `self.get(meta).unwrap().is_unsolved()`
+    * `self.get(meta).is_unsolved()`
     */
     pub fn set(&mut self, meta: Meta, kind: &Kind) {
-        match self.solutions.get_mut(meta) {
-            None => {
-                panic!("meta {:?} not found", meta)
-            }
-            Some(solution) => {
-                if solution.is_unsolved() {
-                    *solution = Solution::Solved(kind.clone());
-                } else {
-                    panic!("meta {:?} has already been set", meta);
-                }
-            }
+        let solution = self
+            .solutions
+            .get_mut(meta)
+            .unwrap_or_else(|| panic!("meta {:?} not found", meta));
+        if solution.is_unsolved() {
+            *solution = Solution::Solved(kind.clone());
+        } else {
+            panic!("meta {:?} has already been set", meta);
         }
     }
 
@@ -74,6 +76,31 @@ impl Solutions {
         let m = self.solutions.len();
         self.solutions.push(Solution::Unsolved);
         m
+    }
+
+    /**
+    Check whether the metavariable occurs in a kind.
+
+    # Preconditions
+
+    * `kind.iter_metas().all(|meta| self.contains(meta))`
+    */
+    pub fn occurs(&self, meta: Meta, kind: &Kind) -> bool {
+        match kind {
+            Kind::Type => false,
+            Kind::Row => false,
+            Kind::Constraint => false,
+            Kind::Meta(other_meta) => {
+                meta == *other_meta
+                    || match self.get(meta) {
+                        Solution::Unsolved => false,
+                        Solution::Solved(kind) => self.occurs(meta, kind),
+                    }
+            }
+            Kind::Ref(kind) => match kind.as_ref() {
+                KindCompound::Arrow(a, b) => self.occurs(meta, a) || self.occurs(meta, b),
+            },
+        }
     }
 
     /**
@@ -103,20 +130,113 @@ impl Solutions {
             Kind::Type => {}
             Kind::Row => {}
             Kind::Constraint => {}
-            Kind::Meta(m) => match self.get(*m) {
-                Some(solution) => match solution {
-                    Solution::Unsolved => {}
-                    Solution::Solved(new_kind) => {
-                        *kind = new_kind.clone();
-                    }
-                },
-                None => {
-                    panic!("meta {:?} not found", m);
+            Kind::Meta(meta) => match self.get(*meta) {
+                Solution::Unsolved => {}
+                Solution::Solved(new_kind) => {
+                    *kind = new_kind.clone();
                 }
             },
             Kind::Ref(kind_ref) => {
                 zonk_compound(self, Rc::make_mut(kind_ref));
             }
         }
+    }
+}
+
+pub enum UnifyError {
+    Mismatch { expected: Kind, actual: Kind },
+    Occurs { meta: Meta, kind: Kind },
+}
+
+impl UnifyError {
+    pub fn mismatch(expected: &Kind, actual: &Kind) -> Self {
+        UnifyError::Mismatch {
+            expected: expected.clone(),
+            actual: actual.clone(),
+        }
+    }
+
+    pub fn occurs(meta: Meta, kind: &Kind) -> Self {
+        UnifyError::Occurs {
+            meta,
+            kind: kind.clone(),
+        }
+    }
+}
+
+pub fn unify(solutions: &mut Solutions, expected: &Kind, actual: &Kind) -> Result<(), UnifyError> {
+    fn solve_left(solutions: &mut Solutions, meta: Meta, actual: &Kind) -> Result<(), UnifyError> {
+        if solutions.occurs(meta, actual) {
+            Err(UnifyError::occurs(meta, actual))
+        } else {
+            solutions.set(meta, actual);
+            Ok(())
+        }
+    }
+
+    fn solve_right(
+        solutions: &mut Solutions,
+        expected: &Kind,
+        meta: Meta,
+    ) -> Result<(), UnifyError> {
+        if solutions.occurs(meta, expected) {
+            Err(UnifyError::occurs(meta, expected))
+        } else {
+            solutions.set(meta, expected);
+            Ok(())
+        }
+    }
+
+    fn unify_meta_left(
+        solutions: &mut Solutions,
+        meta: Meta,
+        actual: &Kind,
+    ) -> Result<(), UnifyError> {
+        match solutions.get(meta).clone() {
+            Solution::Unsolved => solve_left(solutions, meta, actual),
+            Solution::Solved(expected) => unify(solutions, &expected, actual),
+        }
+    }
+
+    fn unify_meta_right(
+        solutions: &mut Solutions,
+        expected: &Kind,
+        meta: Meta,
+    ) -> Result<(), UnifyError> {
+        match solutions.get(meta).clone() {
+            Solution::Unsolved => solve_right(solutions, expected, meta),
+            Solution::Solved(actual) => unify(solutions, expected, &actual),
+        }
+    }
+
+    match expected {
+        Kind::Meta(meta) => unify_meta_left(solutions, *meta, actual),
+        Kind::Type => match actual {
+            Kind::Meta(meta) => unify_meta_right(solutions, expected, *meta),
+            Kind::Type => Ok(()),
+            _ => Err(UnifyError::mismatch(expected, actual)),
+        },
+        Kind::Row => match actual {
+            Kind::Meta(meta) => unify_meta_right(solutions, expected, *meta),
+            Kind::Row => Ok(()),
+            _ => Err(UnifyError::mismatch(expected, actual)),
+        },
+        Kind::Constraint => match actual {
+            Kind::Meta(meta) => unify_meta_right(solutions, expected, *meta),
+            Kind::Constraint => Ok(()),
+            _ => Err(UnifyError::mismatch(expected, actual)),
+        },
+        Kind::Ref(expected_ref) => match expected_ref.as_ref() {
+            KindCompound::Arrow(expected_a, expected_b) => match actual {
+                Kind::Meta(meta) => unify_meta_right(solutions, expected, *meta),
+                Kind::Ref(actual_ref) => match actual_ref.as_ref() {
+                    KindCompound::Arrow(actual_a, actual_b) => {
+                        unify(solutions, expected_a, actual_a)?;
+                        unify(solutions, expected_b, actual_b)
+                    }
+                },
+                _ => Err(UnifyError::mismatch(expected, actual)),
+            },
+        },
     }
 }
