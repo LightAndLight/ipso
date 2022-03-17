7576bf04d0f2dfc81135bd8f5819a9a6fa488ba7
Isaac Elliott <isaace71295@gmail.com>
2022-03-18 07:36:06 +1000

    feat(builtins): implement lt

--- ipso-builtins/src/lib.rs
+++ ipso-builtins/src/lib.rs
@@ -1,6 +1,6 @@
 use ipso_core::{
-    Builtin, ClassDeclaration, ClassMember, CommonKinds, Declaration, Expr, InstanceMember, Module,
-    Type, TypeSig,
+    Branch, Builtin, ClassDeclaration, ClassMember, CommonKinds, Declaration, Expr, InstanceMember,
+    Module, Pattern, Type, TypeSig,
 };
 use ipso_syntax::kind::Kind;
 use std::collections::HashMap;
@@ -835,6 +835,71 @@ pub fn builtins(common_kinds: &CommonKinds) -> Module {
                                     ),
                                     Expr::False,
                                     Expr::True,
+                                ),
+                            ),
+                        ),
+                    )),
+                }
+            },
+            {
+                let a = Type::Var(Kind::Type, 0);
+                Declaration::Definition {
+                    name: String::from("lt"),
+                    // Ord a => a -> a -> Bool
+                    sig: TypeSig::new(
+                        vec![(Rc::from("a"), Kind::Type)],
+                        Type::mk_fatarrow(
+                            common_kinds,
+                            Type::app(
+                                Type::Name(
+                                    Kind::mk_arrow(&Kind::Type, &Kind::Constraint),
+                                    Rc::from("Ord"),
+                                ),
+                                a.clone(),
+                            ),
+                            Type::arrow(
+                                common_kinds,
+                                a.clone(),
+                                Type::arrow(common_kinds, a, Type::Bool),
+                            ),
+                        ),
+                    ),
+                    /*
+                    \ordDict a b ->
+                      case compare ordDict a b of
+                        Less () -> True
+                        _ -> False
+                    */
+                    body: Rc::new(Expr::mk_lam(
+                        true,
+                        Expr::mk_lam(
+                            true,
+                            Expr::mk_lam(
+                                true,
+                                Expr::mk_case(
+                                    Expr::mk_app(
+                                        Expr::mk_app(
+                                            Expr::mk_app(
+                                                Expr::Name(String::from("compare")),
+                                                Expr::Var(2),
+                                            ),
+                                            Expr::Var(1),
+                                        ),
+                                        Expr::Var(0),
+                                    ),
+                                    vec![
+                                        Branch {
+                                            // Less () : (| Equal : (), Greater : (), Less : () |)
+                                            pattern: Pattern::Variant {
+                                                tag: Rc::new(Expr::Int(2)),
+                                            },
+                                            body: Expr::True,
+                                        },
+                                        Branch {
+                                            pattern: Pattern::Wildcard,
+                                            body: Expr::False,
+                                        },
+                                    ],
                                 ),
                             ),
                         ),
