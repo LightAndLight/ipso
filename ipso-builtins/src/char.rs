dd3db46e001c810b16859fff1f7c2319dda14df9
Isaac Elliott <isaace71295@gmail.com>
2022-05-03 19:32:47 +1000

    refactor(builtins): split builtins by ipso module

---
+++ ipso-builtins/src/char.rs
@@ -0,0 +1,20 @@
+use ipso_core::{Builtin, CommonKinds, Declaration, Expr, Type, TypeSig};
+use std::rc::Rc;
+
+pub fn decls(common_kinds: &CommonKinds) -> Vec<Rc<Declaration>> {
+    vec![
+        // eq : Char -> Char -> Bool
+        Rc::new(Declaration::Definition {
+            name: String::from("eq"),
+            sig: TypeSig {
+                ty_vars: Vec::new(),
+                body: Type::arrow(
+                    common_kinds,
+                    Type::Char,
+                    Type::arrow(common_kinds, Type::Char, Type::Bool),
+                ),
+            },
+            body: Expr::alloc_builtin(Builtin::EqChar),
+        }),
+    ]
+}
---
+++ ipso-builtins/src/cmd.rs
@@ -0,0 +1,44 @@
+use ipso_core::{Builtin, CommonKinds, Declaration, Expr, Type, TypeSig};
+use std::rc::Rc;
+
+pub fn decls(common_kinds: &CommonKinds) -> Vec<Rc<Declaration>> {
+    vec![
+        // run : Cmd -> IO ()
+        Rc::new(Declaration::Definition {
+            name: String::from("run"),
+            sig: {
+                TypeSig {
+                    ty_vars: Vec::new(),
+                    body: Type::arrow(
+                        common_kinds,
+                        Type::Cmd,
+                        Type::app(Type::mk_io(common_kinds), Type::Unit),
+                    ),
+                }
+            },
+            body: Expr::alloc_builtin(Builtin::Run),
+        }),
+        // lines : Cmd -> IO (Array String)
+        Rc::new(Declaration::Definition {
+            name: String::from("lines"),
+            sig: TypeSig::new(
+                vec![],
+                Type::arrow(
+                    common_kinds,
+                    Type::Cmd,
+                    Type::app(
+                        Type::mk_io(common_kinds),
+                        Type::app(Type::mk_array(common_kinds), Type::String),
+                    ),
+                ),
+            ),
+            body: Rc::new(Expr::Builtin(Builtin::Lines)),
+        }),
+        // show : Cmd -> String
+        Rc::new(Declaration::Definition {
+            name: String::from("show"),
+            sig: TypeSig::new(vec![], Type::arrow(common_kinds, Type::Cmd, Type::String)),
+            body: Rc::new(Expr::Builtin(Builtin::ShowCmd)),
+        }),
+    ]
+}
