635b23f0f44412614d649257e9d2cda7f78c1806
Isaac Elliott <isaace71295@gmail.com>
2022-05-28 14:38:48 +1000

    refactor(eval): replace convert macros with combinators

--- ipso-eval/src/closure_conversion/mod.rs
+++ ipso-eval/src/closure_conversion/mod.rs
@@ -55,35 +55,39 @@ pub enum Expr {
     Cmd(Vec<CmdPart<Expr>>),
 }
 
-macro_rules! convert_one {
-    ($target:expr, $build:expr) => {{
+pub struct ConvertResult<A> {
+    pub required_vars: FnvHashSet<usize>,
+    pub build: Box<dyn FnOnce(&[usize]) -> A>,
+}
+
+impl<A: 'static> ConvertResult<A> {
+    pub fn closed(value: A) -> ConvertResult<A> {
+        Self {
+            required_vars: FnvHashSet::default(),
+            build: Box::new(move |_| value),
+        }
+    }
+
+    pub fn map<B: 'static, F: 'static + FnOnce(A) -> B>(self, f: F) -> ConvertResult<B> {
         let ConvertResult {
             required_vars,
             build,
-        } = convert_expr($target);
+        } = self;
 
         ConvertResult {
             required_vars,
-            build: Box::new(move |env| {
-                let target = build(env);
-                $build(Rc::new(target))
-            }),
+            build: Box::new(move |env| f(build(env))),
         }
-    }};
-}
+    }
 
-macro_rules! convert_two {
-    ($left:expr, $right:expr, $build:expr) => {{
-        let left = convert_expr($left);
-        let build_left = left.build;
-
-        let right = convert_expr($right);
-        let build_right = right.build;
+    pub fn and<B: 'static>(self, other: ConvertResult<B>) -> ConvertResult<(A, B)> {
+        let build_left = self.build;
+        let build_right = other.build;
 
         let required_by_both = {
             let mut vars = FnvHashSet::default();
-            vars.extend(left.required_vars);
-            vars.extend(right.required_vars);
+            vars.extend(self.required_vars);
+            vars.extend(other.required_vars);
             vars
         };
         ConvertResult {
@@ -91,62 +95,35 @@ macro_rules! convert_two {
             build: Box::new(move |env| {
                 let left = build_left(env);
                 let right = build_right(env);
-                $build(Rc::new(left), Rc::new(right))
+                (left, right)
             }),
         }
-    }};
+    }
 }
 
-macro_rules! convert_three {
-    ($left:expr, $middle:expr, $right:expr, $build:expr) => {{
-        let left = convert_expr($left);
-        let build_left = left.build;
+pub fn convert_many<A: 'static, B: 'static, F: 'static + Fn(&A) -> ConvertResult<B>>(
+    convert_item: F,
+    items: &[A],
+) -> ConvertResult<Vec<B>> {
+    let items: Vec<ConvertResult<_>> = items.iter().map(|item| convert_item(item)).collect();
 
-        let middle = convert_expr($middle);
-        let build_middle = middle.build;
+    let required_by_items = {
+        let mut vars = FnvHashSet::default();
+        items.iter().for_each(|item| {
+            vars.extend(&item.required_vars);
+        });
+        vars
+    };
 
-        let right = convert_expr($right);
-        let build_right = right.build;
-
-        let required_by_all = {
-            let mut vars = FnvHashSet::default();
-            vars.extend(left.required_vars);
-            vars.extend(middle.required_vars);
-            vars.extend(right.required_vars);
-            vars
-        };
-        ConvertResult {
-            required_vars: required_by_all,
-            build: Box::new(move |env| {
-                let left = build_left(env);
-                let middle = build_middle(env);
-                let right = build_right(env);
-                $build(Rc::new(left), Rc::new(middle), Rc::new(right))
-            }),
-        }
-    }};
+    ConvertResult {
+        required_vars: required_by_items,
+        build: Box::new(move |env| items.into_iter().map(|item| (item.build)(env)).collect()),
+    }
 }
 
 pub fn convert(expr: &ipso_core::Expr) -> Expr {
     let result = convert_expr(expr);
     (result.build)(&[])
-}
-
-pub struct ConvertResult<A> {
-    pub required_vars: FnvHashSet<usize>,
-    pub build: Box<dyn FnOnce(&[usize]) -> A>,
-}
-
-impl<A> ConvertResult<A> {
-    pub fn closed(value: A) -> ConvertResult<A>
-    where
-        A: 'static,
-    {
-        Self {
-            required_vars: FnvHashSet::default(),
-            build: Box::new(move |_| value),
-        }
-    }
 }
 
 pub fn convert_expr(expr: &ipso_core::Expr) -> ConvertResult<Expr> {
@@ -240,9 +217,9 @@ pub fn convert_expr(expr: &ipso_core::Expr) -> ConvertResult<Expr> {
         }
 
         // Compound expressions.
-        ipso_core::Expr::App(f, x) => {
-            convert_two!(f, x, Expr::App)
-        }
+        ipso_core::Expr::App(f, x) => convert_expr(f)
+            .and(convert_expr(x))
+            .map(move |(f, x)| Expr::App(Rc::new(f), Rc::new(x))),
         ipso_core::Expr::Let { value, rest } => {
             let value = convert_expr(value);
             let build_value = value.build;
@@ -287,139 +264,69 @@ pub fn convert_expr(expr: &ipso_core::Expr) -> ConvertResult<Expr> {
                 }),
             }
         }
-        ipso_core::Expr::IfThenElse(cond, then_expr, else_expr) => {
-            convert_three!(cond, then_expr, else_expr, Expr::IfThenElse)
-        }
+        ipso_core::Expr::IfThenElse(cond, then_expr, else_expr) => convert_expr(cond)
+            .and(convert_expr(then_expr))
+            .and(convert_expr(else_expr))
+            .map(move |((cond, then_expr), else_expr)| {
+                Expr::IfThenElse(Rc::new(cond), Rc::new(then_expr), Rc::new(else_expr))
+            }),
         ipso_core::Expr::Binop(op, left, right) => {
             let op = *op;
-
-            convert_two!(left, right, |left, right| Expr::Binop(op, left, right))
+            convert_expr(left)
+                .and(convert_expr(right))
+                .map(move |(left, right)| Expr::Binop(op, Rc::new(left), Rc::new(right)))
         }
         ipso_core::Expr::String(parts) => {
-            let parts: Vec<ConvertResult<StringPart<Expr>>> =
-                parts.iter().map(|part| convert_string_part(part)).collect();
+            convert_many(convert_string_part, parts).map(Expr::String)
+        }
+        ipso_core::Expr::Array(items) => convert_many(convert_expr, items).map(Expr::Array),
+        ipso_core::Expr::Extend(index, value, record) => convert_expr(index)
+            .and(convert_expr(value))
+            .and(convert_expr(record))
+            .map(move |((index, value), record)| {
+                Expr::Extend(Rc::new(index), Rc::new(value), Rc::new(record))
+            }),
+        ipso_core::Expr::Record(fields) => convert_many(
+            |field: &(ipso_core::Expr, ipso_core::Expr)| {
+                convert_expr(&field.0).and(convert_expr(&field.1))
+            },
+            fields,
+        )
+        .map(Expr::Record),
 
-            let required_by_parts = {
-                let mut vars = FnvHashSet::default();
-                parts.iter().for_each(|part| {
-                    vars.extend(&part.required_vars);
-                });
-                vars
-            };
-            ConvertResult {
-                required_vars: required_by_parts,
-                build: Box::new(move |env| {
-                    let parts = parts.into_iter().map(|part| (part.build)(env)).collect();
-                    Expr::String(parts)
-                }),
-            }
+        ipso_core::Expr::Project(record, index) => convert_expr(record)
+            .and(convert_expr(index))
+            .map(move |(record, index)| Expr::Project(Rc::new(record), Rc::new(index))),
+        ipso_core::Expr::Variant(tag) => {
+            convert_expr(tag).map(move |tag| Expr::Variant(Rc::new(tag)))
         }
-        ipso_core::Expr::Array(items) => {
-            let items: Vec<ConvertResult<Expr>> =
-                items.iter().map(|item| convert_expr(item)).collect();
-
-            let required_by_items = {
-                let mut vars = FnvHashSet::default();
-                items.iter().for_each(|item| {
-                    vars.extend(&item.required_vars);
-                });
-                vars
-            };
-            ConvertResult {
-                required_vars: required_by_items,
-                build: Box::new(move |env| {
-                    let items = items.into_iter().map(|item| (item.build)(env)).collect();
-                    Expr::Array(items)
-                }),
-            }
-        }
-        ipso_core::Expr::Extend(index, value, record) => {
-            convert_three!(index, value, record, Expr::Extend)
-        }
-        ipso_core::Expr::Record(fields) => {
-            let fields: Vec<(ConvertResult<Expr>, ConvertResult<Expr>)> = fields
-                .iter()
-                .map(|(index, value)| (convert_expr(index), convert_expr(value)))
-                .collect();
-
-            let required_by_fields = {
-                let mut vars = FnvHashSet::default();
-                fields.iter().for_each(|(index, value)| {
-                    vars.extend(&index.required_vars);
-                    vars.extend(&value.required_vars);
-                });
-                vars
-            };
-            ConvertResult {
-                required_vars: required_by_fields,
-                build: Box::new(move |env| {
-                    let fields = fields
-                        .into_iter()
-                        .map(|(index, value)| {
-                            let index = (index.build)(env);
-                            let value = (value.build)(env);
-
-                            (index, value)
-                        })
-                        .collect();
-                    Expr::Record(fields)
-                }),
-            }
-        }
-        ipso_core::Expr::Project(record, index) => {
-            convert_two!(record, index, Expr::Project)
-        }
-        ipso_core::Expr::Variant(tag) => convert_one!(tag, Expr::Variant),
-        ipso_core::Expr::Embed(tag, variant) => convert_two!(tag, variant, Expr::Embed),
+        ipso_core::Expr::Embed(tag, variant) => convert_expr(tag)
+            .and(convert_expr(variant))
+            .map(move |(tag, variant)| Expr::Embed(Rc::new(tag), Rc::new(variant))),
         ipso_core::Expr::Case(expr, branches) => {
             let expr = convert_expr(expr);
             let build_expr = expr.build;
 
-            let branches: Vec<ConvertResult<Branch<Expr>>> = branches
-                .iter()
-                .map(|branch| convert_branch(branch))
-                .collect();
+            let branches = convert_many(convert_branch, branches);
+            let build_branches = branches.build;
 
             let required_by_case = {
                 let mut vars = FnvHashSet::default();
                 vars.extend(expr.required_vars);
-                branches.iter().for_each(|branch| {
-                    vars.extend(&branch.required_vars);
-                });
+                vars.extend(branches.required_vars);
                 vars
             };
             ConvertResult {
                 required_vars: required_by_case,
                 build: Box::new(move |env| {
                     let expr = build_expr(env);
-                    let branches = branches
-                        .into_iter()
-                        .map(|branch| (branch.build)(env))
-                        .collect();
+                    let branches = build_branches(env);
 
                     Expr::Case(Rc::new(expr), branches)
                 }),
             }
         }
-        ipso_core::Expr::Cmd(parts) => {
-            let parts: Vec<ConvertResult<CmdPart<Expr>>> =
-                parts.iter().map(|part| convert_cmd_part(part)).collect();
-
-            let required_by_parts = {
-                let mut vars = FnvHashSet::default();
-                parts.iter().for_each(|item| {
-                    vars.extend(&item.required_vars);
-                });
-                vars
-            };
-            ConvertResult {
-                required_vars: required_by_parts,
-                build: Box::new(move |env| {
-                    let parts = parts.into_iter().map(|part| (part.build)(env)).collect();
-                    Expr::Cmd(parts)
-                }),
-            }
-        }
+        ipso_core::Expr::Cmd(parts) => convert_many(convert_cmd_part, parts).map(Expr::Cmd),
 
         // Simple expressions.
         ipso_core::Expr::Name(name) => ConvertResult::closed(Expr::Name(name.clone())),
@@ -449,18 +356,7 @@ pub fn convert_string_part(part: &StringPart<ipso_core::Expr>) -> ConvertResult<
             let string = string.clone();
             ConvertResult::closed(StringPart::String(string))
         }
-        StringPart::Expr(expr) => {
-            let expr = convert_expr(expr);
-            let build_expr = expr.build;
-
-            ConvertResult {
-                required_vars: expr.required_vars,
-                build: Box::new(move |env| {
-                    let expr = build_expr(env);
-                    StringPart::Expr(expr)
-                }),
-            }
-        }
+        StringPart::Expr(expr) => convert_expr(expr).map(StringPart::Expr),
     }
 }
 
@@ -470,15 +366,7 @@ pub fn convert_cmd_part(part: &CmdPart<ipso_core::Expr>) -> ConvertResult<CmdPar
             let string = string.clone();
             ConvertResult::closed(CmdPart::Literal(string))
         }
-        CmdPart::Expr(expr) => {
-            let expr = convert_expr(expr);
-            let build_expr = expr.build;
-
-            ConvertResult {
-                required_vars: expr.required_vars,
-                build: Box::new(move |env| CmdPart::Expr(build_expr(env))),
-            }
-        }
+        CmdPart::Expr(expr) => convert_expr(expr).map(CmdPart::Expr),
     }
 }
 
@@ -530,28 +418,10 @@ pub fn convert_pattern(pattern: &Pattern<ipso_core::Expr>) -> ConvertResult<Patt
     match pattern {
         ipso_core::Pattern::Record { names, rest } => {
             let rest = *rest;
-
-            let names: Vec<ConvertResult<Expr>> =
-                names.iter().map(|name| convert_expr(name)).collect();
-
-            let required_by_names = {
-                let mut vars = FnvHashSet::default();
-                names
-                    .iter()
-                    .for_each(|name| vars.extend(&name.required_vars));
-                vars
-            };
-            ConvertResult {
-                required_vars: required_by_names,
-                build: Box::new(move |env| {
-                    let names: Vec<Expr> =
-                        names.into_iter().map(|name| (name.build)(env)).collect();
-                    Pattern::Record { names, rest }
-                }),
-            }
+            convert_many(convert_expr, names).map(move |names| Pattern::Record { names, rest })
         }
         ipso_core::Pattern::Variant { tag } => {
-            convert_one!(tag, |tag| Pattern::Variant { tag })
+            convert_expr(tag).map(move |tag| Pattern::Variant { tag: Rc::new(tag) })
         }
         ipso_core::Pattern::Name => ConvertResult::closed(Pattern::Name),
         ipso_core::Pattern::Char(c) => ConvertResult::closed(Pattern::Char(*c)),
