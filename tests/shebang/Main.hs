43551edaece36039cda8f7d38cffce29471f2445
Isaac Elliott <isaace71295@gmail.com>
2022-07-24 19:34:17 +1000

    build(golden,shebang): remove Option, bump base bound

--- tests/shebang/Main.hs
+++ tests/shebang/Main.hs
@@ -6,7 +6,6 @@ import Test.Ipso.Common (Config(..), displayError, eqExitCode, examplesMain, run
 import Control.Monad (unless)
 import Data.Foldable (for_)
 import Data.IORef (modifyIORef, newIORef, readIORef)
-import Data.Semigroup (Option (..))
 import Data.Text (Text)
 import qualified Data.Text as Text
 import qualified Dhall
@@ -42,7 +41,7 @@ runExample config example = do
       []
       (maybe "" Text.unpack $ exStdin example)
 
-  errorRef <- newIORef $ Option Nothing
+  errorRef <- newIORef Nothing
 
   mStdoutDiff <- runDiff (exStdout example) (Text.pack stdout)
   for_ mStdoutDiff $ \diff ->
@@ -65,7 +64,7 @@ runExample config example = do
   unless (eqExitCode (exExitCode example) exitCode) $ do
     modifyIORef errorRef (<> pure (displayError "exitcode" (exPath example) (exExitCode example, Text.pack . show) (exitCode, show)))
 
-  Option mError <- readIORef errorRef
+  mError <- readIORef errorRef
   pure $
     case mError of
       Just err -> Left err
