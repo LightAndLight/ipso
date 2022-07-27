9cc97264963290c828fae399037eaa803aba253e
Isaac Elliott <isaace71295@gmail.com>
2022-07-28 08:14:26 +1000

    feat: add path.exists (#219)

---
+++ ipso-builtins/src/path.rs
@@ -0,0 +1,22 @@
+use ipso_core::{Builtin, CommonKinds, Declaration, Expr, Type, TypeSig};
+use std::rc::Rc;
+
+pub fn decls(common_kinds: &CommonKinds) -> Vec<Rc<Declaration>> {
+    vec![
+        // exists : String -> IO Bool
+        Rc::new(Declaration::Definition {
+            name: String::from("exists"),
+            sig: {
+                TypeSig {
+                    ty_vars: vec![],
+                    body: Type::arrow(
+                        common_kinds,
+                        Type::String,
+                        Type::app(Type::mk_io(common_kinds), Type::Bool),
+                    ),
+                }
+            },
+            body: Expr::alloc_builtin(Builtin::PathExists),
+        }),
+    ]
+}
